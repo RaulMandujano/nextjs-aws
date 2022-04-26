be0400835c92225343c28a58a1ad05ae03203523
Raul <raulmandujanoleon@gmail.com>
2022-04-26 16:04:31 -0600

    s

--- amplify-backup/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "breakingbadproject": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
